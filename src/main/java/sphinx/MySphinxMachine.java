204eb92958b0a9727c665b49e37aca38d328db47
Tharindu Rusira <tharindurusira@gmail.com>
2014-03-28 07:59:04 +0530

    added resource files and path modifications to the code

--- src/main/java/sphinx/MySphinxMachine.java
+++ src/main/java/sphinx/MySphinxMachine.java
@@ -3,6 +3,9 @@ package sphinx;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
@@ -13,18 +16,19 @@ public class MySphinxMachine {
 	public static void main(String[] args) {
 		
 		Configuration c= new Configuration();
-		c.setAcousticModelPath("resource:/WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz");
-		c.setDictionaryPath("resource:/WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz/dict/cmudict.0.6d");
-		c.setLanguageModelPath("models/language/en-us.lm.dmp");
+		c.setAcousticModelPath("src/main/resources/TIDIGITS_8gau_13dCep_16k_40mel_130Hz_6800Hz");
+		c.setDictionaryPath("src/main/resources/WSJ_8gau_13dCep_8k_31mel_200Hz_3500Hz");
+		c.setLanguageModelPath("src/main/resources/en-us.lm.dmp");
 		
 		try {
 			StreamSpeechRecognizer rec = new StreamSpeechRecognizer(c);
-			rec.startRecognition(new FileInputStream(new File("")));
+			rec.startRecognition(new FileInputStream(new File("src/main/resources/man1_nb.wav")));
+			rec.stopRecognition();
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
 		
 	}
 
